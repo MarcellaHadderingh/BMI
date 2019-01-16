a4ff5fb3127bbd0500302e43f8e2ef2ee80c62c1
marcella <marcellavanduyn@gmail.com>
2019-01-16 10:22:52 +0100

    changed >
    25>28

--- bmi.php
+++ bmi.php
@@ -24,6 +24,8 @@ class mens{
     }
 }
 
+
+
 $m1 = new Mens("Piet", 1.67, 63.5);
 $m2 = new Mens("Klaas", 1.83, 100.0);
 $m3 = new Mens("Jan", 1.93, 103 );
@@ -31,10 +33,13 @@ $m3 = new Mens("Jan", 1.93, 103 );
 $klas = [$m1, $m2, $m3];
 
 foreach($klas as $m)
-if($m->geefmejebmi() > 28){
+if($m->geefmejebmi() > 25){
 echo "<p style='color:red;'>" . "<B>" . $m->geefmejenaam() . " " . round ($m->geefmejebmi(),1) . "</B>" . "<BR>"; 
 }
 else
 echo "<p style='color:green;'>" . $m->geefmejenaam() . " " . round ($m->geefmejebmi(),1) . "<BR>"; 
 
+
+
 ?>
+
